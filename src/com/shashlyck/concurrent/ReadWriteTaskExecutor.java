f5a77bce25e534df8d5f5350a95eb12c68f6e348
shashlyck <garlodey2@mail.ru>
2020-02-22 22:03:24 +0400

    3

--- src/com/shashlyck/concurrent/ReadWriteTaskExecutor.java
+++ src/com/shashlyck/concurrent/ReadWriteTaskExecutor.java
@@ -6,7 +6,8 @@ import com.shashlyck.functions.TabulatedFunction;
 
 public class ReadWriteTaskExecutor {
     public static void main(String[] args) {
-        TabulatedFunction linkedListTabulatedFunction = new LinkedListTabulatedFunction(new ConstantFunction(-5), 1, 1000, 1000);
+        TabulatedFunction linkedListTabulatedFunction =
+                new LinkedListTabulatedFunction(new ConstantFunction(-5), 1, 1000, 1000);
         Thread readThread = new Thread(new ReadTask(linkedListTabulatedFunction));
         Thread writeThread = new Thread(new WriteTask(linkedListTabulatedFunction, 11));
         writeThread.start();
